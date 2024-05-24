dffa513812f00228ff3dd5a26a4ab7b47b7d4fb1
kakalexander <kauanalexander123@hotmail.com>
2024-05-24 19:33:25 -0300

    Falta fzr responsividade

--- script.js
+++ script.js
@@ -37,3 +37,18 @@ function showContent(buttonNumber) {
 document.addEventListener('DOMContentLoaded', function() {
   showContent(1);
 });
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+setInterval(() => {
+  proximaImg()
+},5000)
+function proximaImg(){
+  cont++
+
+  if(cont > 3 ){
+    cont = 1
+  }
+  document.getElementById('radio'+cont).checked = true
+}
